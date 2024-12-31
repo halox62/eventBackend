c24af628ef66715934814d629987dabd996805c8
ThetaLog(n) <giorgiomartucci02@gmail.com>
2024-12-31 14:57:56 +0100

    Update configuration

--- app.py
+++ app.py
@@ -29,11 +29,22 @@ from dotenv import load_dotenv
 
 #psql -U postgres
 
-load_dotenv('/Users/giorgiomartucci/Documents/OutfitApp/key.env')
+#load_dotenv('/Users/giorgiomartucci/Documents/OutfitApp/key.env')
+
+#FIREBASE_PUBLIC_KEYS_URL = os.getenv("FIREBASE_PUBLIC_KEYS_URL")
+#FIREBASE_PROJECT_ID = os.getenv("FIREBASE_PROJECT_ID")
+
 
 FIREBASE_PUBLIC_KEYS_URL = os.getenv("FIREBASE_PUBLIC_KEYS_URL")
 FIREBASE_PROJECT_ID = os.getenv("FIREBASE_PROJECT_ID")
+FIREBASE_CREDENTIALS = os.getenv("FIREBASE_CREDENTIALS")
 
+
+print(FIREBASE_PUBLIC_KEYS_URL)
+print(FIREBASE_PROJECT_ID)
+
+
+credentials_dict = json.loads(FIREBASE_CREDENTIALS)
 
 app = Flask(__name__)
 
